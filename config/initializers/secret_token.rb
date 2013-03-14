4dd22da122dc6a51af3f6fb80c0334b5c1db93ce
ShaB <shab@ubuntu.(none)>
2013-03-14 14:45:41 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Airbraketest::Application.config.secret_token = '01f186e535a38a63702ac1f982833c53051e07f74a1dbbd60ca2b9d6de9198e5eb0a859671c9e3ec5e05f98954ac44881580882d50ad98c8eeb9a6d9167bc025'
